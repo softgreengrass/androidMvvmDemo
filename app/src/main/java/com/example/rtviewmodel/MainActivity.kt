497281d1a1bc801bd334964e1e125cd94414073a
wang <softgreengrass@163.com>
2020-04-03 15:44:23 +0800

    Update MainActivity.kt
    fdfdfffdfd

--- app/src/main/java/com/example/rtviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/rtviewmodel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rtviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProviders
 
 
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createViewModel() {
+        Log.d("1","fdjfjdfjfj")
     }
 }
